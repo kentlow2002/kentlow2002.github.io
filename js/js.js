bbaecf39d9a1930b2bee671db4f9e7205ca6cad7
kent low <kentlow2002@gmail.com>
2019-08-27 18:03:42 +0800

    Added projects.html and updated index.html

--- js/js.js
+++ js/js.js
@@ -9,24 +9,15 @@ function shadow()
   }
 }
 //check url for strng and bold correct option on navbar
-if(window.location.href.indexOf("index.html")>=0) {
-  document.getElementById("home").className="nav-link active";
+if(window.location.href.indexOf("projects")>=0) {
+  document.getElementById("projects").className="nav-link active";
 }
-else if(window.location.href.indexOf("recipe.html")>=0) {
-  document.getElementById("recipe").className="nav-link active";
+else if(window.location.href.indexOf("journey")>=0) {
+  document.getElementById("journey").className="nav-link active";;
 }
-else if(window.location.href.indexOf("calc.html")>=0) {
-  document.getElementById("calc").className="nav-link active";;
-}
-else if(window.location.href.indexOf("contact.html")>=0) {
+else if(window.location.href.indexOf("contact")>=0) {
   document.getElementById("contact").className="nav-link active";
 }
-else if(window.location.href.indexOf("about.html")>=0) {
+else if(window.location.href.indexOf("about")>=0) {
   document.getElementById("about").className="nav-link active";
-}
-else if(window.location.href.indexOf("thank.html")>=0) {
-
-}
-else {
-  document.getElementById("home").className="nav-link active";
 }
